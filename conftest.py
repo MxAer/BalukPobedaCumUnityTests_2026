7fea05b0cb8ff8b208bb389f80446e4498585416
linuxfight <kiko13024@gmail.com>
2026-01-21 13:46:44 +0300

    refactor: restructure auth tests, add enum/rfc checks, ensure unique emails

--- conftest.py
+++ conftest.py
@@ -1,15 +1,17 @@
 import logging
 from collections.abc import MutableMapping
 from typing import Any
-from faker import Faker
+
 import pytest
+from faker import Faker
 
 fake = Faker()
+
 
 def pytest_tavern_beta_before_every_request(request_args: MutableMapping):
     message = f"Request: {request_args['method']} {request_args['url']}"
 
-    params = request_args.get('params', None)
+    params = request_args.get("params", None)
     if params:
         message += f"\nQuery parameters: {params}"
 
@@ -17,9 +19,15 @@ def pytest_tavern_beta_before_every_request(request_args: MutableMapping):
 
     logging.info(message)
 
+
 def pytest_tavern_beta_after_every_response(expected: Any, response: Any) -> None:
     logging.info(f"Response: {response.status_code} {response.text}")
 
+
 @pytest.fixture
 def generate_random_email() -> str:
-    return fake.email()
+    return fake.unique.email()
+
+@pytest.fixture
+def generate_secondary_email() -> str:
+    return fake.unique.email()
